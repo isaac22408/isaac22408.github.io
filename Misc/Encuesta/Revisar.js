f818d3550411d176d0d686711c1151bf4ce091c7
isaac22408 <isaac22408@users.noreply.github.com>
2024-10-12 08:15:52 -0700

    nose

---
+++ Misc/Encuesta/Revisar.js
@@ -0,0 +1,8 @@
+function sumit(){
+    alert("Callate diego")
+}
+const words = new  URLSearchParams(window.location.search)
+function si(){
+words.get("genere")
+alert (words)
+}
---
+++ Repository/defauld.js
@@ -0,0 +1,6 @@
+function start(){
+    const head = document.head
+    head.append(document.createElement("title"))
+    document.getElementsByTagName("title")[0].append("Omega Cat Studio")
+    console.log(document.getElementById("defauld").innerHTML + "yes")
+}
