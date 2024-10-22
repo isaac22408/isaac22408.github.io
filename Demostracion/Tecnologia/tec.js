f8a9f98fcae6119e09c9b613b6199de7a9e3bcf8
isaac22408 <isaac22408@users.noreply.github.com>
2024-10-21 19:12:31 -0700

    deteccion reparada

--- Demostracion/Tecnologia/tec.js
+++ Demostracion/Tecnologia/tec.js
@@ -1,60 +1,63 @@
 let sWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 let sHeight = (window.innerHeight > 0) ? window.innerHeight : screen.height;
-const mover = (o) =>{
+
+const mover = (o) => {
     o.style.top = `${100 + Math.floor(Math.random() * 500)}px`;
     o.style.left = `${Math.floor(Math.random() * sWidth)}px`;
-    o.style.rotate = `${Math.floor(Math.random() * 360)}deg` 
-}
-const cuadros = []
-for (let i = 0; i < 10; i++){
+    o.style.rotate = `${Math.floor(Math.random() * 360)}deg`;
+};
+
+const cuadros = [];
+for (let i = 0; i < 10; i++) {
     cuadros.push(document.getElementById("zona-movimiento").appendChild(document.createElement("div")));
     cuadros[i].classList.add("cuadrado");
     cuadros[i].style.top = `${100 + Math.floor(Math.random() * 500)}px`;
     cuadros[i].style.left = `${Math.floor(Math.random() * sWidth)}px`;
 }
-zona = document.getElementById("zona-movimiento");
+
+const zona = document.getElementById("zona-movimiento");
+const servicio = document.getElementById("servicios");
+
 const oPantalla = {
     root: null,
     rootMargin: "0px",
-    threshold: 0.8,
-  };
-  const emf = (entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            mover(entry.target);
-        }
-    });
+    threshold: 0.1,
 };
 
 let atencion = true;
+
 const f = (entries) => {
     entries.forEach(entry => {
         if (!entry.isIntersecting) {
             atencion = false;
-            console.log("no")
+            console.log("no");
         }
     });
 };
+
 const t = (entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             atencion = true;
-            console.log("si")
+            console.log("si");
         }
     });
 };
+
 const para = new IntersectionObserver(f, oPantalla);
 const sigue = new IntersectionObserver(t, oPantalla);
+
 sigue.observe(zona);
-para.observe(document.getElementById("servicios"));
-document.getElementById("zona-movimiento").children
+para.observe(zona);
+
 const intervalId = setInterval(() => {
     if (atencion) {
-        Array.from(document.getElementById("zona-movimiento").children).forEach(i => {
+        Array.from(zona.children).forEach(i => {
             mover(i);
         });
     }
 }, 5000);
+
 const Lista = document.getElementById("mover").children[1];
 const bi = document.getElementById("bi");
 const bd = document.getElementById("bd");
@@ -76,7 +79,10 @@ bd.addEventListener('click', () => {
     ver = (ver < Lista.children.length - 1) ? ver + 1 : 0;
     checa();
 });
+
 setInterval(() => {
     ver = (ver < Lista.children.length - 1) ? ver + 1 : 0;
-    checa();}, 5000);
+    checa();
+}, 5000);
+
 checa();
